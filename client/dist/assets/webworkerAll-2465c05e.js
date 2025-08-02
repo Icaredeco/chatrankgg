ec9d7d838502f947f6799dce0263182d2e35a387
= <121827489+Icaredeco@users.noreply.github.com>
2025-08-02 16:16:30 +0200

    trying to correct render

--- client/dist/assets/webworkerAll-2465c05e.js
+++
@@ -1,1 +0,0 @@
-import"./init-232b4797.js";import"./index-cce13090.js";import"./colorToUniform-21d3a5e1.js";
---
+++ client/dist/assets/webworkerAll-4f855f45.js
@@ -0,0 +1,1 @@
+import"./init-6f3a8b7a.js";import"./index-d92e987c.js";import"./colorToUniform-21d3a5e1.js";

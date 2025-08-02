30545c4d372a3f250467446b744d6a881a144eb7
= <121827489+Icaredeco@users.noreply.github.com>
2025-08-02 18:48:27 +0200

    HomePage.jsx location correction

--- server/routes/streamers.js
+++ server/routes/streamers.js
@@ -1,14 +1,14 @@
 // routes/streamers.js
 
 import express from 'express'
-import Player from '../models/User.js' // Assure-toi que ce modèle existe bien
+import User from '../models/User.js' // Assure-toi que ce modèle existe bien
 
 const router = express.Router()
 
 // Route pour récupérer tous les streamers avec au moins un joueur
 router.get('/streamers', async (req, res) => {
   try {
-    const data = await Player.aggregate([
+    const data = await User.aggregate([
       { $group: { _id: "$streamerCode", count: { $sum: 1 } } },
       { $sort: { count: -1 } }
     ])
